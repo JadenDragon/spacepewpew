76fc4f4fdef4a61ef28c69366b2755d32c0a26f7
Jogum Cannegieter <joqum.C@gmail.com>
2025-03-04 15:41:08 +0100

    bullet
    started the bullet scene and implement most of the shoot functions.

---
+++ Scripts/bullet.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var bulletSpeed = 800
+
+func _physics_process(delta):
+	global_position.y += -bulletSpeed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	pass # Replace with function body.
+	queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,25 @@
 extends CharacterBody2D
 
-@export var speed = 300
+signal bullet_shot
+
+@export var shipSpeed = 300
+
+var laser_scene = preload("res://Scenes/bullet.tscn")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot")
+		shoot()
 
 #physics process cause character is a physics body
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), 
 							Input.get_axis("move_up", "move_down"))
 	print(direction)
-	velocity = direction * speed
+	velocity = direction * shipSpeed	
 	move_and_slide()
+
+func shoot():
+	pass
